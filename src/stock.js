d2b7752bcb39802efad2f1e00ab5b95480bb332a
trena <20079502+tholst952@users.noreply.github.com>
2025-06-27 14:37:45 -0700

    more chair card icons

--- src/stock.js
+++ src/stock.js
@@ -726,7 +726,7 @@ export default function CurrentStock() {
                     />
                   </svg>
 
-                  <span>Jigsaws & </span>
+                  <span>Luncheon & Jigsaws </span>
                 </li>
                 <li>
                   <svg
@@ -786,7 +786,7 @@ export default function CurrentStock() {
 
               <div className="chair-price">
                 <span>
-                  <strong>$640</strong>
+                  <strong>$449</strong>
                 </span>
                 <btn className="btn--small">Add to cart</btn>
               </div>
@@ -881,7 +881,7 @@ export default function CurrentStock() {
 
               <div className="chair-price">
                 <span>
-                  <strong>$640</strong>
+                  <strong>$380</strong>
                 </span>
                 <btn className="btn--small">Add to cart</btn>
               </div>
@@ -897,25 +897,25 @@ export default function CurrentStock() {
             </div>
 
             <div className="stock-card-contents">
-              <h4 className="stock-card-title">The Blank Blank</h4>
+              <h4 className="stock-card-title">Something Here</h4>
               <ul className="stock-card-list">
                 <li>
-                  <span>Quiet reflection</span>
+                  <span>something here</span>
                 </li>
                 <li>
-                  <span>Comfortable for 6 months</span>
+                  <span>Comfortable for a time</span>
                 </li>
                 <li>
-                  <span>Natural polyester</span>
+                  <span>something here</span>
                 </li>
                 <li>
-                  <span>Weighs 41 lbs.</span>
+                  <span>something here</span>
                 </li>
               </ul>
 
               <div className="chair-price">
                 <span>
-                  <strong>$640</strong>
+                  <strong>$39</strong>
                 </span>
                 <btn className="btn--small">Add to cart</btn>
               </div>
@@ -945,7 +945,7 @@ export default function CurrentStock() {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M12 9v3.75m0-10.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.25-8.25-3.286Zm0 13.036h.008v.008H12v-.008Z"
+                      d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
                     />
                   </svg>
 
@@ -1003,13 +1003,13 @@ export default function CurrentStock() {
                     />
                   </svg>
 
-                  <span>Weighs 5 lbs.</span>
+                  <span>Weighs 5lbs/2.2kg</span>
                 </li>
               </ul>
 
               <div className="chair-price">
                 <span>
-                  <strong>$640</strong>
+                  <strong>$45</strong>
                 </span>
                 <btn className="btn--small">Add to cart</btn>
               </div>
@@ -1025,16 +1025,25 @@ export default function CurrentStock() {
             </div>
 
             <div className="stock-card-contents">
-              <h4 className="stock-card-title">The Fringe Duchess</h4>
+              <h4 className="stock-card-title">The Vertigo Cure</h4>
               <ul className="stock-card-list">
                 <li>
-                  <span>Quiet reflection</span>
-                </li>
-                <li>
-                  <span>Comfortable for 6 months</span>
-                </li>
-                <li>
-                  <span>Natural polyester</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+
+                  <span>Reading & Writing</span>
                 </li>
                 <li>
                   <svg
@@ -1048,17 +1057,53 @@ export default function CurrentStock() {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z"
+                      d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
                     />
                   </svg>
 
-                  <span>Weighs 41 lbs.</span>
+                  <span>Comfortable for a Lifetime</span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+                    />
+                  </svg>
+
+                  <span>Easy-Off Legs</span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+                    />
+                  </svg>
+
+                  <span>Weighs 272 oz.</span>
                 </li>
               </ul>
 
               <div className="chair-price">
                 <span>
-                  <strong>$640</strong>
+                  <strong>$99</strong>
                 </span>
                 <btn className="btn--small">Add to cart</btn>
               </div>
@@ -1074,16 +1119,25 @@ export default function CurrentStock() {
             </div>
 
             <div className="stock-card-contents">
-              <h4 className="stock-card-title">The Slanted Trotter</h4>
+              <h4 className="stock-card-title">The Tickled Trotter</h4>
               <ul className="stock-card-list">
                 <li>
-                  <span>Leaning & People Watching</span>
-                </li>
-                <li>
-                  <span>Comfortable for 2-3 Hours</span>
-                </li>
-                <li>
-                  <span>Steel Pipe Frame</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                    />
+                  </svg>
+
+                  <span>Phone Calls & People Watching</span>
                 </li>
                 <li>
                   <svg
@@ -1097,11 +1151,47 @@ export default function CurrentStock() {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z"
+                      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                     />
                   </svg>
 
-                  <span>Weighs 29 lbs.</span>
+                  <span>Comfortable for 2-3 Hours</span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 12.75 6 6 9-13.5"
+                    />
+                  </svg>
+
+                  <span>Hollow Steel Pipe Frame</span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                    />
+                  </svg>
+
+                  <span>Heft Level Easy</span>
                 </li>
               </ul>
 
@@ -1123,16 +1213,61 @@ export default function CurrentStock() {
             </div>
 
             <div className="stock-card-contents">
-              <h4 className="stock-card-title">The Fringe Duchess</h4>
+              <h4 className="stock-card-title">The Big Cheese</h4>
               <ul className="stock-card-list">
                 <li>
-                  <span>Quiet reflection</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                    />
+                  </svg>
+
+                  <span>Melancholia & Sunbathing</span>
                 </li>
                 <li>
-                  <span>Comfortable for 6 months</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                    />
+                  </svg>
+
+                  <span>Comfortable for 6 Months</span>
                 </li>
                 <li>
-                  <span>Natural polyester</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="icon"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+                    />
+                  </svg>
+
+                  <span>Delicate Shalloon Covering</span>
                 </li>
                 <li>
                   <svg
@@ -1150,13 +1285,13 @@ export default function CurrentStock() {
                     />
                   </svg>
 
-                  <span>Weighs 41 lbs.</span>
+                  <span>Approx 0.0275 Tonnage</span>
                 </li>
               </ul>
 
               <div className="chair-price">
                 <span>
-                  <strong>$640</strong>
+                  <strong>$500</strong>
                 </span>
                 <btn className="btn--small">Add to cart</btn>
               </div>
